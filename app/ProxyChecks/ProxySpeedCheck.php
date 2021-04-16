4ce772da127eea71ce58851e359857a720f20448
YSRoot <ualeroev@bk.ru>
2021-04-16 18:10:12 +0300

    feat: add download speed check

--- app/ProxyChecks/ProxySpeedCheck.php
+++ app/ProxyChecks/ProxySpeedCheck.php
@@ -4,13 +4,29 @@ namespace App\ProxyChecks;
 
 use App\Models\Proxy;
 use Closure;
+use GuzzleHttp\Client;
+use Illuminate\Http\File;
+use Illuminate\Support\Carbon;
 
 class ProxySpeedCheck
 {
     public function handle(Proxy $proxy, Closure $next)
     {
-        echo 'i check speed';
-        //скачать несколько раз что-то записывая время и посчитать среднюю скрость
+        $client = new Client();
+        $downloadTimeRow = [];
+        $tmpfile = tempnam(sys_get_temp_dir(),'dl');
+        foreach (range(0, 5) as $it) {
+            $startTime = Carbon::now();
+            $client->request('GET', 'https://stluciadance.com/prospectus_file/sample.pdf', [
+                'sink' => $tmpfile,
+            ]);
+            $diffRow[] = Carbon::now()->diffInSeconds($startTime);
+        }
+        $size = (new File($tmpfile))->getSize() / 1024; //convert to KB
+        $avgDownloadTime = array_sum($downloadTimeRow) / count($downloadTimeRow);
+
+        $proxy->speed = $size / $avgDownloadTime;
+
         return $next($proxy);
     }
 }
