9b28954d29fd67d9d945655723b771f861805a22
rootis69 <ualeroev@bk.ru>
2021-04-17 23:59:38 +0300

    chore: set default route redirect to proxy-list create
    Удалил welcome страницу, перенаправил сразу на страницу заполнения прокси

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProxyController;
 use App\Http\Controllers\ProxyListController;
 use Illuminate\Support\Facades\Route;
@@ -15,13 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::resource('proxy', ProxyController::class)->only(['index', 'show']);
 Route::resource('proxy-lists', ProxyListController::class)->only(['show', 'create', 'store']);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::redirect('/', route('proxy-lists.create'));
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
